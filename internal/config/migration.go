cf319ea8ce30bee4397bdb258a8d7793b5817af5
ZefanyaPrasetiyo <zefanyaprasetiyo29@gmail.com>
2026-09-14 23:00:27 +0700

    feat: add model Event, and configure enum query in go migration

--- internal/config/migration.go
+++ internal/config/migration.go
@@ -14,10 +14,17 @@ func MigrateDB(db *gorm.DB) {
 				CREATE TYPE user_role AS ENUM ('admin', 'staff', 'buyer'); 
 			END IF; 
 		END $$;
+
+		DO $$ 
+		BEGIN 
+			IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'event_status') THEN 
+				CREATE TYPE event_status AS ENUM ('draft', 'published', 'ended'); 
+			END IF; 
+		END $$;
 	`
 	db.Exec(enumQuery)
 
-	err := db.AutoMigrate(&models.User{}, &models.Venue{}, &models.Artist{})
+	err := db.AutoMigrate(&models.User{}, &models.Venue{}, &models.Artist{}, &models.Event{})
 	if err != nil {
 		log.Fatal("Gagal migrasi model:", err)
 	}
---
+++ internal/models/events.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type EventStatus string
+
+const (
+	EventDraft     EventStatus = "draft"
+	EventPublished EventStatus = "published"
+	EventEnded     EventStatus = "ended"
+)
+
+type Event struct {
+	ID          uuid.UUID   `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	ArtistID    uuid.UUID   `gorm:"type:uuid;not null" json:"artist_id"`
+	VenueID     uuid.UUID   `gorm:"type:uuid;not null" json:"venue_id"`
+	Title       string      `gorm:"type:varchar(200);not null" json:"title"`
+	Description string      `gorm:"type:text;not null" json:"description"`
+	StartDate   time.Time   `gorm:"not null" json:"start_date"`
+	Status      EventStatus `gorm:"type:event_status;default:'draft';not null" json:"status"`
+	BannerURL   *string     `gorm:"type:varchar(255)" json:"banner_url,omitempty"`
+	Artist Artist `gorm:"foreignKey:ArtistID" json:"artist"`
+	Venue  Venue  `gorm:"foreignKey:VenueID" json:"venue"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
